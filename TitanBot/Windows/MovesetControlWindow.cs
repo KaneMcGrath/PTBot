f0fea6a4b213271bf5cd4622fd2dd70d852e813a
Kane McGrath <kane@kanemcgrath.com>
2022-11-12 04:28:50 -0800

    Moveset Control Window Style Changes

--- TitanBot/Windows/MovesetControlWindow.cs
+++ TitanBot/Windows/MovesetControlWindow.cs
@@ -50,18 +50,24 @@ namespace TitanBot.Windows
                         CheckForChanges();
                     }
                     ControlWindow.OnGUI();
-                    Rect ScrollArea = new Rect(ControlWindow.ContentRect.x, ControlWindow.ContentRect.y, ControlWindow.ContentRect.width, ControlWindow.ContentRect.height - 40f);
-                    Rect BottomArea = new Rect(ControlWindow.ContentRect.x, ControlWindow.ContentRect.y + ControlWindow.ContentRect.height - 40f, ControlWindow.ContentRect.width, 40f);
+                    Rect ScrollArea = new Rect(ControlWindow.ContentRect.x, ControlWindow.ContentRect.y, ControlWindow.ContentRect.width, ControlWindow.ContentRect.height - 50f);
+                    Rect BottomArea = new Rect(ControlWindow.ContentRect.x, ControlWindow.ContentRect.y + ControlWindow.ContentRect.height - 32f, ControlWindow.ContentRect.width, 30f);
+                    Rect WarningArea = new Rect(ControlWindow.ContentRect.x + 2f, ControlWindow.ContentRect.y + ControlWindow.ContentRect.height - 80f, ControlWindow.ContentRect.width - 4f, 50f);
 
                     for (int i = 0; i < movesToControl.Length; i++)
                     {
                         MovesetControlElement(i, movesToControl[i]);
                     }
                     ScrollList.DrawBlanks(ScrollArea);
+                    FlatUI.Label(WarningArea, "Warning! setting StartAt too high on certain moves can cause spaming explosions which will disconnect you from the game.  Try out moves in offline mode first", WarningTextStyle);
                     if (changesCount > 0)
                     {
-                        FlatUI.Label(RTools.SplitH(BottomArea, 3, 0, 2), changesCount.ToString() + " unsaved changes");
-                        if (FlatUI.Button(RTools.SplitH(BottomArea, 3, 2), "Apply Changes"))
+                        FlatUI.Label(RTools.SplitH(BottomArea, 8, 0, 4), changesCount.ToString() + " unsaved changes", FlatUI.titleStyle);
+                        if (FlatUI.Button(RTools.SplitH(BottomArea, 8, 4, 2), "Undo Changes"))
+                        {
+                            UpdateWindowData();
+                        }
+                        if (FlatUI.Button(RTools.SplitH(BottomArea, 8, 6, 2), "Apply Changes"))
                         {
                             ApplyChanges();
                         }
@@ -78,12 +84,13 @@ namespace TitanBot.Windows
             {
                 GUI.DrawTexture(new Rect(r.x, r.y, r.width, 2f), FlatUI.outsideColorTex);
                 FlatUI.Label(ScrollList.IndexToRect(idx, 8, 0, 4), modifiableMovesetControls[move].Name, FlatUI.titleStyle);
-                moveEnabled[move] = FlatUI.Check(ScrollList.IndexToRect(idx, 8, 4, 3), moveEnabled[move], "Enabled");
+                
             }
             if (ScrollList.IsVisible(idx + 1))
             {
                 FlatUI.Label(ScrollList.IndexToRect(idx + 1, 8, 0, 2), "Start At: ");
                 startAnimAtTextBoxs[move] = FlatUI.TextField(ScrollList.IndexToRect(idx + 1, 8, 2, 2), startAnimAtTextBoxs[move]);
+                moveEnabled[move] = FlatUI.Check(ScrollList.IndexToRect(idx + 1, 12, 7, 4), moveEnabled[move], "Move Enabled");
             }
         }
         public static void UpdateWindowData()
@@ -150,5 +157,37 @@ namespace TitanBot.Windows
             }
             CGTools.log("Moveset Updated for All Titans!");
         }
+
+        private static void UndoChanges()
+        {
+            foreach (PTAction action in movesToControl)
+            {
+                if (PlayerTitanBot.TempActionsList.Contains(action) && !moveEnabled[action])
+                {
+                    moveEnabled[action] = true;
+                }
+                if (!PlayerTitanBot.TempActionsList.Contains(action) && moveEnabled[action])
+                {
+                    moveEnabled[action] = false;
+                }
+                
+                if (!modifiableMovesetControls[action].Equals(MovesetControl.movesetControlDatabase[action]))
+                {
+                    changesCount++;
+                }
+
+            }
+        }
+
+        public static GUIStyle WarningTextStyle = new GUIStyle
+        {
+            border = new RectOffset(1, 1, 1, 1),
+            alignment = TextAnchor.UpperLeft,
+            fontSize = 14,
+            fontStyle = FontStyle.Bold,
+            clipping = TextClipping.Overflow,
+            wordWrap = true,
+            normal = { textColor = Color.red }
+        };
     }
 }
